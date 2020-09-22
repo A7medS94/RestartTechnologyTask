c79304caa1ccb685255af9867f6e59e02b2b00aa
Ahmed Samir <a.samir_94@hotmail.com>
2020-09-22 21:05:11 +0200

    Initial Commit

---
+++ RestartTechnologyTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestartTechnologyTask
+//
+//  Created by Ahmed Samir on 9/22/20.
+//  Copyright © 2020 Ahmed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
